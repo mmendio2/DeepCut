cde18ab5a3b4417927ae20457ac1020c2acfdd0c
mmendio2 <mmendio2@trinity.edu>
2022-04-05 17:18:38 -0500

    mostly working frontpage (looks nice) + SEARCH

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import SearchBar from "./Components/SearchBar";
+function App() {
+  return <div className="App">
+    <SearchBar placeholder = "Enter a movie!"/>
+  </div>;
+}
+
+
+export default App;
